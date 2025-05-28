bf361eaf4471d25ef32912191b64d4ef1916bc60
Alexander Stamnes <alesta81@yahoo.com>
2025-05-28 14:31:33 +0200

    authentication completed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,8 @@
 class ListingsController < ApplicationController
 
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:index]
-  # skip_before_action :authenticate_user!, only: [:index, :show]
-  
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     # örnek: @listings = Listing.all
   end
