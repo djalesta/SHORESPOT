6deb1db0115a99cd717dcc6f682507e333ad139f
Alexander Stamnes <alesta81@yahoo.com>
2025-05-28 14:10:45 +0200

    authentication almost complete

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
   # skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
