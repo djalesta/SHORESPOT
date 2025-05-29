d9c5c5c500c3f27e0fd7015e2dd61e79c21d2398
Zeynep ÇaCaliskan <zeynepcaliskan@hacettepe.edu.tr>
2025-05-29 18:49:19 +0300

    routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  get 'extras/new'
+
   devise_for :users
 
+  root to: "listings#index"
+  resources :listings do
+    resources :bookings, only: [:new, :create, :index]
+    resources :extras, only: [:new]
+  end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "sellerlistings", to: "listings#sellerlistings", as: :seller_listings
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
