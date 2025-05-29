55465784c560a4dc1a48eeb90d363e715e4cb055
Zeynep ÇaCaliskan <zeynepcaliskan@hacettepe.edu.tr>
2025-05-29 18:25:43 +0300

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :listings # config/routes.rb
+  resources :listings 
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
