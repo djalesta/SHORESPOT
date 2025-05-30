1e51fe7cc929383bdff87e384839f000ccf0e53f
Zeynep ÇaCaliskan <zeynepcaliskan@hacettepe.edu.tr>
2025-05-30 15:56:19 +0300

    stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import SelectionController from "./selection_controller"
-application.register("selection", SelectionController)
